7c5b91c9196aa37ce19efce7577c9f5ff31fdfec
Bob F <bobfdev@gmail.com>
2022-05-25 22:35:31 -0500

    updated files

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,26 +6,26 @@ import newyork from './images/newyork.jpeg';
 export const Data = [
     {
         image : paris,
-        text : 'Paris ,  France',
-        status: 'Enjoy Our one of the Biggest Tour in Paris Just Clicking one Button',
+        text : 'Paris, France',
+        status: 'Enjoy one of the finest cities in Europe',
         className : 'data-items'
     },
     {
         image : barcelona,
-        text : 'Barcelona ,  Spain',
-        status: 'Enjoy Our one of the Biggest Tour in Barcelona Just Clicking one Button',
+        text : 'Barcelona, Spain',
+        status: 'Enjoy one of the finest cities in Europe',
         className : 'data-items'
     },
     {
         image : moscow,
-        text : 'Moscow ,  Russia',
-        status: 'Enjoy Our one of the Biggest Tour in Moscow Just Clicking one Button',
+        text : 'Moscow, Russia',
+        status: 'Enjoy one of the finest cities in Europe',
         className : 'data-items'
     },
     {
         image : newyork,
-        text : 'New York ,  USA',
-        status: 'Enjoy Our one of the Biggest Tour in New York Just Clicking one Button',
+        text : 'New York, USA',
+        status: 'Enjoy one of the finest cities in the North America',
         className : 'data-items'
     }
 ]
